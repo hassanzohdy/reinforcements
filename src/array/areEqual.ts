29867e3e290bcf470ed7c651f625c2c612558cfc
hasan <hassanzohdy@gmail.com>
2022-09-12 23:05:00 +0200

    Enhanced function parameters types

--- src/array/areEqual.ts
+++ src/array/areEqual.ts
@@ -1,13 +1,6 @@
 /**
  * Determine if the given arrays are equal to each other
- *
- * @param {array} array1
- * @param {array} array2
- * @return {boolean}
  */
-export default function areEqual<T>(
-  array1: Array<T>,
-  array2: Array<T>,
-): boolean {
+export default function areEqual(array1: any[], array2: any[]): boolean {
   return JSON.stringify(array1.sort()) === JSON.stringify(array2.sort());
 }
